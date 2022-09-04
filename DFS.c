d64643976009c131580869e519cabf0932a1d945
prajwalramagoudapatil <101803815+prajwalramagoudapatil@users.noreply.github.com>
2022-09-04 11:54:33 +0530

    Update DFS.c

--- DFS.c
+++ DFS.c
@@ -6,54 +6,33 @@ typedef struct node {
  struct node *next;
  int vertex;
 }node;
+
 node *G[20]; 
 //heads of linked list
 int visited[20];
 int n;
 int q[20], f=0, r=-1;
 
-
-
 void read_graph(); 
-//create adjacency list
-void insert(int
-,int); 
-//insert an edge(vi,vj) in the adjacency list
+   //create adjacency list
+void insert(int,int); 
+   //insert an edge(vi,vj) in the adjacency list
 void DFS(int);
-void BFS(int);
 void printAdjList();
-
-int stack[20], top=-1, v[20];
-void topologicalSort(int v) {
-  node *p, *q;
-  
-  p=G[v];
-  visited[v] = 1;
-  while( !v[p->vertex] ) {
-    topologicalSort(p->vertex);
-  }
-  
-  stack[++top] = v;
-  
-}
 
 void main() {
  int i;
  read_graph();
- //initialised visited to 0
  
+ //initialised visited to 0
  for(i=0;i<n;i++)
  visited[i]=0;
+ 
  printf("adj list : \n");
  printAdjList();
- 
 
  DFS(0);
- printf("\nafter dfs bef BFS\n");
- for(i=0;i<n;i++)
- visited[i]=0;
- BFS(0);
- printf("rama rama hare hare hare krishna hare krishna krishna krishna hare krishna");
+ 
 }
 
 void DFS(int i)
@@ -63,38 +42,31 @@ void DFS(int i)
  printf("\n%d",i);
  p=G[i];
  visited[i]=1;
- while(p!=NULL)
- {
- i=p->vertex;
- 
- if(!visited[i])
- DFS(i);
- p=p->next;
+ while(p!=NULL) {
+  i=p->vertex;
+  if(!visited[i])
+  DFS(i);
+  p=p->next;
  }
 }
 
-
-void read_graph()
-{
+void read_graph() {
  int i,vi,vj,no_of_edges;
  printf("Enter number of vertices:");
  
  scanf("%d",&n);
  //initialise G[] with a null
  
- for(i=0;i<n;i++)
- {
- G[i]=NULL;
- //read edges and insert them in G[]
- 
- printf("Enter number of edges:");
- scanf("%d",&no_of_edges);
- for(i=0;i<no_of_edges;i++)
- {
- printf("Enter an edge(u,v):");
- scanf("%d%d",&vi,&vj);
- insert(vi,vj);
- }
+ for(i=0;i<n;i++) {
+  G[i]=NULL;
+   //read edges and insert them in G[]
+   printf("Enter number of edges:");
+   scanf("%d",&no_of_edges);
+   for(i=0;i<no_of_edges;i++){
+     printf("Enter an edge(u,v):");
+     scanf("%d%d",&vi,&vj);
+     insert(vi,vj);
+   }
  }
 }
 
@@ -107,18 +79,15 @@ void insert(int vi,int vj)
  q->vertex=vj;
  q->next=NULL;
  //insert the node in the linked list number vi
- 
-
  if(G[vi]==NULL)
- G[vi]=q;
- else
- {
- //go to end of the linked list
- p=G[vi];
+  G[vi]=q;
+ else {
+  //go to end of the linked list
+  p=G[vi];
  
- while(p->next!=NULL)
- p=p->next;
- p->next=q;
+  while(p->next!=NULL)
+  p=p->next;
+  p->next=q;
  }
 }
 
